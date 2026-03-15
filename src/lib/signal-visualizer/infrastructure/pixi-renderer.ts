a74541468b9ca61b3633e9c8fdeb7f65c0867f63
BritoAlv <gomzalezbritoalvaro@gmail.com>
2026-03-15 18:58:23 -0400

    added grid system with axes

--- src/lib/signal-visualizer/infrastructure/pixi-renderer.ts
+++ src/lib/signal-visualizer/infrastructure/pixi-renderer.ts
@@ -2,8 +2,8 @@ import {
     type Renderer,
     type RenderModel
 } from "@/lib/signal-visualizer/core/Renderer.ts";
-import {Application, Container, Graphics} from "pixi.js";
-import {normalizeCoords} from "@/lib/signal-visualizer/utils/utils.ts";
+import {Application, Container, Graphics, Text} from "pixi.js";
+import {Envelope} from "@/lib/signal-visualizer/utils/utils.ts";
 
 export class PixiRenderer implements Renderer {
     private readonly _canvas: HTMLCanvasElement;
@@ -37,75 +37,124 @@ export class PixiRenderer implements Renderer {
         const data = model.oneDimSignals
         const coordinateSystem = new Container()
 
-        const xCordsNormalized = normalizeCoords(data.samples)
-        const yValuesNormalized = data.channels.map(channel => normalizeCoords(channel))
+        const xValues = new Envelope(data.samples)
+        const yValuesPerChannel = data.channels.map(channel => new Envelope(channel))
 
         const marginHorizontal = plotHeight * 0.05
-        const marginVertical = plotWidth * 0.05
-
+        const marginVerticalLeft = plotWidth * 0.15
+        const marginVerticalRight = plotWidth * 0.05
+        const xDivisions = 10
+        const yDivisions = 4
+        const xCord = 0
+        const xLeft = xCord + marginVerticalLeft
+        const xRight = xCord + this.width - marginVerticalRight
+        const widthAfterMargin = (xRight - xLeft)
         for (let i = 0; i < totalChannels; i++) {
-            const xCord = 0
             const yCord = i * plotHeight
-
-            const xLeft = xCord + marginVertical
-            const xRight = xCord + this.width - marginVertical
-
             const yLow = yCord + marginHorizontal
             const yHigh = yCord + plotHeight - marginHorizontal
-
-            const widthAfterMargin = (xRight - xLeft)
             const heightAfterMargin = (yHigh - yLow)
 
-            const plot = this.draw1DPlot(xLeft, yLow, widthAfterMargin, heightAfterMargin, xCordsNormalized, yValuesNormalized[i]!)
+            const channelRect = new Graphics()
+                .rect(xCord, yCord, plotWidth, plotHeight)
+                .stroke({width: 2, color: 'orange'})
+
+            coordinateSystem.addChild(channelRect)
+
+            const plot = this.draw1DPlot(widthAfterMargin, heightAfterMargin, xValues, yValuesPerChannel[i]!, xDivisions, yDivisions)
+            plot.x = xLeft
+            plot.y = yLow
             coordinateSystem.addChild(plot)
         }
-
-        const xAxisGraphic = this.drawXAxis(marginVertical, this.height - xAxisHeight, this.width - 2 * marginVertical, xAxisHeight, xCordsNormalized)
-        coordinateSystem.addChild(xAxisGraphic)
+        const xAxisY = this.height - xAxisHeight
+        const xAxisRect = new Graphics()
+            .rect(xCord, xAxisY, plotWidth, xAxisHeight)
+            .stroke({width: 2, color: 'orange'})
+        const xAxisGraphic = this.drawXAxis(widthAfterMargin, xAxisHeight, xValues.min, xValues.max, 10)
+        xAxisGraphic.x = xLeft
+        xAxisGraphic.y = xAxisY
+        coordinateSystem.addChild(xAxisGraphic, xAxisRect)
         this.app.stage.addChild(coordinateSystem)
     }
 
-
-    private drawXAxis(xOffset: number, yOffset: number, width: number, height: number, xCoords: Float32Array): Graphics {
+    private drawXAxis(width: number, height: number, xMin: number, xMax: number, divisions: number): Graphics {
         const graphics = new Graphics()
-            .rect(xOffset, yOffset, width, height)
-            .stroke({width: 4, color: 'red'})
+            .rect(0, 0, width, height)
+            .stroke({width: 2, color: 'red'})
 
-        const n = xCoords.length
-        const yCoordinate = yOffset + height / 2
-        for(let i = 0; i < n; i++)
-        {
-            graphics.circle(xOffset + xCoords[i]! * width, yCoordinate, 4)
-            graphics.stroke({width: 4, color: 'green'})
+        const yCoordinate = height * 0.2
+        const stepSize = (xMax - xMin) / divisions
+        for (let i = 0; i <= divisions; i++) {
+            const xDivision = (i / divisions) * width
+            graphics.circle(xDivision, yCoordinate, 4)
+            graphics.stroke({width: 2, color: 'green'})
+            const text = new Text(
+                {
+                    text: (xMin + i * (stepSize)).toPrecision(2),
+                    style: {
+                        fontSize: height * 0.40,
+                        fontWeight: 'bold',
+                    }
+                }
+            )
+            text.x = xDivision - text.width / 2
+            text.y = yCoordinate
+
+            graphics.addChild(text)
         }
 
         return graphics
     }
 
-    private draw1DPlot(xOffset: number, yOffset: number, width: number, height: number, xValuesNormalized: Float32Array, yValuesNormalized: Float32Array): Graphics {
+    private draw1DPlot(width: number, height: number, xValues: Envelope, yValues: Envelope, xDivisions: number, yDivisions: number): Graphics {
         const graphics = new Graphics().rect(
-            xOffset, yOffset, width, height
-        ).stroke({width: 4, color: 'black'})
+            0, 0, width, height
+        ).stroke({width: 2, color: 'black'})
 
-        const n = yValuesNormalized.length
+        const n = yValues.length
         const xCoords = new Float32Array(n)
         const yCoords = new Float32Array(n)
 
+        for (let i = 0; i <= xDivisions; i++) {
+            const xDivision = (i / xDivisions) * width
+            graphics.moveTo(xDivision, height).lineTo(xDivision, 0).stroke({color: 'red', width: 1})
+        }
+
+        const stepSize = (yValues.max - xValues.min) / yDivisions
+        for (let i = 0; i <= yDivisions; i++) {
+            const yDivision = (i / yDivisions) * height
+            graphics
+                .moveTo(0, yDivision)
+                .lineTo(width, yDivision).stroke({color: 'red', width: 1})
+            const text = new Text(
+                {
+                    text: (yValues.max - i * stepSize).toPrecision(2),
+                    style: {
+                        fontSize: height * 0.10,
+                        fontWeight: 'bold',
+                    }
+                }
+            )
+            text.x = - 1.3 * text.width
+            text.y = yDivision - text.height / 3
+
+            graphics.addChild(text)
+        }
+
         for (let i = 0; i < n; i++) {
-            const xMappedCord = width * xValuesNormalized[i]!
-            const yMappedCord = height * yValuesNormalized[i]!
+            const xMappedCord = width * xValues.normalized[i]!
+            const yMappedCord = height * yValues.normalized[i]!
 
-            xCoords[i] = xOffset + xMappedCord
-            yCoords[i] = yOffset + (height)  - yMappedCord
+            xCoords[i] = xMappedCord
+            yCoords[i] = height - yMappedCord
 
-            graphics.circle(xCoords[i]!, yCoords[i]!, width * 0.01).stroke(
+            graphics.circle(xCoords[i]!, yCoords[i]!, width * 0.005).stroke(
                 {color: 'green'}
             )
-
             if (i > 0) {
                 graphics.moveTo(xCoords[i - 1]!, yCoords[i - 1]!)
                 graphics.lineTo(xCoords[i]!, yCoords[i]!)
-                graphics.stroke({color: 'pink', width: 3})
+                graphics.stroke({color: 'pink', width: 1})
             }
         }
         return graphics
@@ -115,7 +164,7 @@ export class PixiRenderer implements Renderer {
         await this.app.init({
             width: width,
             height: height,
-            preference: 'webgl',
+            preference: 'webgpu',
             canvas: this._canvas,
             backgroundAlpha: 0,
         })
--- src/lib/signal-visualizer/utils/utils.ts
+++ src/lib/signal-visualizer/utils/utils.ts
@@ -1,38 +1,47 @@
 export class Envelope {
     minValues: Float32Array
     maxValues: Float32Array
+    normalized: Float32Array
+    array: Float32Array
 
-    constructor(minValues: Float32Array, maxValues: Float32Array) {
+    get length(): number {
+        return this.array.length
+    }
+
+    get min() {
+        return this.minValues[this.minValues.length! - 1]!
+    }
+
+    get max() {
+        return this.maxValues[this.maxValues.length! - 1]!
+    }
+
+    constructor(array: Float32Array) {
+        this.array = array
+
+        const minValues = new Float32Array(array.length)
+        const maxValues = new Float32Array(array.length)
+
+        if (array.length > 0) {
+            minValues[0] = array[0]!
+            for (let i = 1; i < array.length; i++) {
+                const next: number = array[i]!
+                minValues[i] = Math.min(next, minValues[i - 1]!)
+                maxValues[i] = Math.max(next, maxValues[i - 1]!)
+            }
+        }
         this.maxValues = maxValues
         this.minValues = minValues
+        this.normalized = normalizeCoords(this.array, this.min, this.max)
     }
 }
 
-export function computeEnvelope(array: Float32Array) {
-    const minValues = new Float32Array(array.length)
-    const maxValues = new Float32Array(array.length)
-
-    if (array.length > 0) {
-        minValues[0] = array[0]!
-        for (let i = 1; i < array.length; i++) {
-            const next: number = array[i]!
-            minValues[i] = Math.min(next, minValues[i - 1]!)
-            maxValues[i] = Math.max(next, maxValues[i - 1]!)
-        }
-    }
-
-    return new Envelope(minValues, maxValues)
-}
-
-export function normalizeCoords(samples: Float32Array): Float32Array {
-    const xEnvelope = computeEnvelope(samples)
+function normalizeCoords(samples: Float32Array, min: number, max: number): Float32Array {
     const n = samples.length
-    const xMin = xEnvelope.minValues[n - 1]!
-    const xMax = xEnvelope.maxValues[n - 1]!
     const normalized = new Float32Array(n)
     for (let i = 0; i < n; i++) {
         const sample = samples[i]!
-        normalized[i] = (sample - xMin) / (xMax - xMin)
+        normalized[i] = (sample - min) / (max - min)
     }
     return normalized
 }
