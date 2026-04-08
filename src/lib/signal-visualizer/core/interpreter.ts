23b9e32e4283ee995ead81e7a13b12f087d90145
BritoAlv <gomzalezbritoalvaro@gmail.com>
2026-04-08 11:12:51 -0400

    implemented a data manager to remove from the interpreter the functions data related

--- src/lib/signal-visualizer/core/interpreter.ts
+++ src/lib/signal-visualizer/core/interpreter.ts
@@ -1,40 +1,35 @@
-import { type SignalSource, type SignalSourceBuildData } from '@/lib/signal-visualizer/application/types/signalSource.ts'
+import { type SignalSourceBuildData } from '@/lib/signal-visualizer/application/types/signalSource.ts'
 
 import type {
-    NormalizedSignal,
     OneDimNormalizedSignal,
 } from '@/lib/signal-visualizer/core/types.ts'
 import { RenderManager } from '@/lib/signal-visualizer/core/renderManager.ts'
-import { Envelope } from '@/lib/signal-visualizer/utils/envelope.ts'
-import { largestTriangleThreeBuckets } from '../utils/lttb'
 import { ViewPort } from '@/lib/signal-visualizer/application/types/viewPort.ts'
-import { MockSignalSourceBuilder, type MockSignalSourceConstructor } from '../infrastructure/signals/mockSignalSource'
+import { DataManager } from './dataManager/dataManager'
 
 export class Interpreter {
     private renderer: RenderManager
-    private readonly signalsSources: Record<string, SignalSource>
+    private dataManager: DataManager
+    private labels: string[]
     private viewPort: ViewPort
 
     constructor(renderer: RenderManager, viewPort: ViewPort, signalsSourceBuildData: SignalSourceBuildData[]) {
         this.viewPort = viewPort
-        /*
-        This code is not solid, does not follow ocp.
-        */
-        this.signalsSources = signalsSourceBuildData.reduce<Record<string, SignalSource>>((acc, buildData) => {
-            if (buildData.signalSourceType === 'MockSignalSource') {
-                const builder = new MockSignalSourceBuilder()
-                acc[buildData.label] = builder.build(buildData as MockSignalSourceConstructor)
-            }
-            return acc
-        }, {})
         this.renderer = renderer
+        this.labels = signalsSourceBuildData.map(x => x.label)
+        this.dataManager = new DataManager(signalsSourceBuildData)
+    }
+
+    private get expectedWidth(): number {
+        return Math.floor(this.renderer.sizeData.width * this.renderer.devicePixelRatio)
+    }
+
+    private fetchData(labels: string[]) {
+        return this.dataManager.fetchData(labels, this.viewPort, this.expectedWidth)
     }
 
     async init() {
-        const data: OneDimNormalizedSignal[] = []
-        for (const label in this.signalsSources) {
-            data.push(await this.fetchData(label))
-        }
+        const data = await this.fetchData(this.labels)
         await this.renderer.init(data, this.viewPort)
     }
 
@@ -42,46 +37,11 @@ export class Interpreter {
         this.renderer.destroy()
     }
 
-    private async fetchData(label: string): Promise<OneDimNormalizedSignal> {
-        const sizeData = this.renderer.getChannelSizeData()
-        const viewPort = this.viewPort
-        const signalSource = this.signalsSources[label]!
-        const data = await signalSource.read(viewPort)
-        const expectedWidth = Math.floor(sizeData.width * this.renderer.devicePixelRatio)
-        const dataToUse = largestTriangleThreeBuckets(data, expectedWidth)
-        const xEnvelope = new Envelope(dataToUse.xValues, {
-            min: this.viewPort.startSeconds,
-            max: this.viewPort.startSeconds + this.viewPort.lengthSeconds,
-        })
-        const xAxisSignal: NormalizedSignal = {
-            values: xEnvelope.normalized,
-            minMaxValues: {
-                min: xEnvelope.min,
-                max: xEnvelope.max,
-            },
-        }
-        const yEnvelope = new Envelope(dataToUse.yValues)
-        const yAxisSignal: NormalizedSignal = {
-            values: yEnvelope.normalized,
-            minMaxValues: {
-                min: yEnvelope.min,
-                max: yEnvelope.max,
-            },
-        }
-        return Promise.resolve({
-            label: signalSource.label,
-            xSignal: xAxisSignal,
-            ySignal: yAxisSignal,
-        })
-    }
 
     async updateChannelsState(): Promise<void> {
         const viewPort = this.viewPort
         const activeChannels = this.renderer.visibleChannels
-        const updatedData: OneDimNormalizedSignal[] = []
-        for (const label of activeChannels) {
-            updatedData.push(await this.fetchData(label))
-        }
+        const updatedData: OneDimNormalizedSignal[] = await this.fetchData(activeChannels)
         await this.renderer.updateSignalData(updatedData, viewPort)
     }
 
@@ -105,8 +65,8 @@ export class Interpreter {
         if (!visibility) {
             this.renderer.removeChannel(channelLabel)
         } else {
-            const signal = await this.fetchData(channelLabel)
-            this.renderer.addChannel(signal)
+            const signals = (await this.fetchData([channelLabel]))
+            this.renderer.addChannel(signals[0]!)
         }
     }
 }
