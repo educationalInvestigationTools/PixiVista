a4ee2767c4b4739fe887bc08f25404c7a3aba292
BritoAlv <gomzalezbritoalvaro@gmail.com>
2026-04-20 11:59:30 -0400

    removed repeated renderManager

--- src/lib/signal-visualizer/metricsComponent/core/metricsContainer.ts
+++ src/lib/signal-visualizer/metricsComponent/core/metricsContainer.ts
@@ -1,5 +1,5 @@
 import { EventMediator } from '@/lib/signal-visualizer/utils/eventMediator.ts'
-import { MetricsRenderManager } from '@/lib/signal-visualizer/metricsComponent/core/metricsRenderManager.ts'
+import { RenderManager } from '@/lib/signal-visualizer/core/rendering/renderManager.ts'
 import { MetricsComponentApi } from '@/lib/signal-visualizer/metricsComponent/core/metricsComponentApi.ts'
 import { DEFAULT_ROLLING_WINDOW_MS } from '@/lib/signal-visualizer/metricsComponent/application/types/rollingWindowConfig.ts'
 
@@ -11,7 +11,7 @@ export class MetricsContainer {
     }
 
     async init(htmlElement: HTMLElement, rollingWindowMs: number = DEFAULT_ROLLING_WINDOW_MS) {
-        const renderManager = new MetricsRenderManager(htmlElement, this.eventMediator)
+        const renderManager = new RenderManager(htmlElement, this.eventMediator)
         const componentApi = new MetricsComponentApi(
             renderManager.sizeData,
             this.eventMediator,
