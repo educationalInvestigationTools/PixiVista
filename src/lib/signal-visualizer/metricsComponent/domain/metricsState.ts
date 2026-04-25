e5f1dbb9a743feb176b61726c3e0da1c9da02bae
BritoAlv <gomzalezbritoalvaro@gmail.com>
2026-04-24 21:09:28 -0400

    fixed bug due to division by zero

--- src/lib/signal-visualizer/metricsComponent/domain/metricsState.ts
+++ src/lib/signal-visualizer/metricsComponent/domain/metricsState.ts
@@ -1,3 +1,4 @@
+import type { Point2D } from "../../core/types/Point2d"
 import { clamp } from "../../utils/utils"
 import type { MetricsPoints } from "../infrastructure/rendering/componentLayer/metricsComponentLayer"
 import type { PointsData } from "./types/pointsData"
@@ -11,9 +12,11 @@ export type MetricsSample = {
 export class MetricsState {
     private samples: MetricsSample[] = []
     private readonly windowMs: number
+    private readonly outputSamples: number
 
-    constructor(windowMs: number) {
+    constructor(windowMs: number, outputSamples = 200) {
         this.windowMs = windowMs
+        this.outputSamples = outputSamples
     }
 
     pushSample(sample: MetricsSample) {
@@ -56,6 +59,36 @@ export class MetricsState {
         }
     }
 
+    private downSamplingAlgorithm(points: Point2D[]): Point2D[] {
+        const delta = 1 / (this.outputSamples - 1)
+        const times = []
+        const values: [number, number][] = []
+        for (let i = 0; i < this.outputSamples; i++) {
+            times.push(i * delta)
+            values.push([0, 0])
+        }
+        for (const point of points) {
+            let group = 0
+            for (let i = 0; i < this.outputSamples; i++){
+                if (Math.abs(point.x - times[group]!) > Math.abs(point.x - times[i]!)) {
+                    group = i
+                }
+            }
+            values[group]![0] += point.y
+            values[group]![1]++
+        }
+
+        const result: Point2D[] = []
+        for (let i = 0; i < this.outputSamples; i++) {
+            const x = times[i]!
+            const y = values[i]![1] === 0 ? 0 : values[i]![0] / values[i]![1]
+            result.push({
+                x, y
+            })
+        }
+        return result
+    }
+
     private buildWindowPoints(
         samples: MetricsSample[],
         getValue: (sample: MetricsSample) => number,
@@ -72,18 +105,19 @@ export class MetricsState {
                 currentValue: 0,
             }
         }
+
         const latestTimestamp = samples[samples.length - 1]!.timestampMs
         const cutoff = latestTimestamp - this.windowMs
         const points = samples.map((sample) => ({
             x: clamp((sample.timestampMs - cutoff) / this.windowMs, 0, 1),
             y: getValue(sample),
         }))
-
-
-        const maxValue = points.reduce((maxValue, point) => Math.max(maxValue, point.y), 0)
-        const currentValue = points[points.length - 1]!.y
+        const downSampledPoints = this.downSamplingAlgorithm(points)
+        const maxValue = downSampledPoints.reduce((maxValue, point) => Math.max(maxValue, point.y), 0)
+        const currentValue = downSampledPoints[downSampledPoints.length - 1]!.y
+        console.log(downSampledPoints.length)
         return {
-            points: points,
+            points: downSampledPoints,
             minValue: 0,
             maxValue: maxValue,
             currentValue: currentValue,
