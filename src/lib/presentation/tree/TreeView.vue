40bae50b4f6d7cb52f39e95b15cf90da7eb7fe22
BritoAlv <gomzalezbritoalvaro@gmail.com>
2026-05-31 09:25:00 -0400

    remove unused slot props

--- src/lib/presentation/tree/TreeView.vue
+++ src/lib/presentation/tree/TreeView.vue
@@ -10,12 +10,6 @@ defineOptions({ name: 'TreeView' })
 
 type TreeSlotProps<TNode extends TreeNodeLike<TNode>> = {
     node: TNode
-    depth: number
-    isLast: boolean
-    ancestorHasNext: boolean[]
-    hasChildren: boolean
-    isCollapsed: boolean
-    toggleCollapse: () => void
 }
 
 const props = defineProps<{
@@ -102,43 +96,17 @@ function toggleCollapse() {
 <template>
     <div class="tree-node">
         <div ref="rowElement" class="tree-node__row">
-            <TreeConnector
-                v-if="depth > 0"
-                :isLast="isLast"
-                :depth="depth"
-                :ancestorHasNext="ancestorHasNext" />
-            <TreeRow
-                :hasChildren="hasChildren"
-                :isCollapsed="isCollapsed"
-                :toggleCollapse="toggleCollapse">
-                <slot
-                    :node="props.node"
-                    :depth="depth"
-                    :isLast="isLast"
-                    :ancestorHasNext="ancestorHasNext"
-                    :hasChildren="hasChildren"
-                    :isCollapsed="isCollapsed"
-                    :toggleCollapse="toggleCollapse" />
+            <TreeConnector v-if="depth > 0" :isLast="isLast" :depth="depth" :ancestorHasNext="ancestorHasNext" />
+            <TreeRow :hasChildren="hasChildren" :isCollapsed="isCollapsed" :toggleCollapse="toggleCollapse">
+                <slot :node="props.node" />
             </TreeRow>
         </div>
 
         <div v-if="hasChildren && !isCollapsed" class="tree-node__children">
-            <TreeView
-                v-for="(child, index) in children"
-                :key="child.id"
-                :node="child"
-                :depth="childDepth"
-                :ancestorHasNext="childAncestorHasNext"
-                :isLast="index === children.length - 1">
-                <template #default="{ node, depth, isLast, ancestorHasNext, hasChildren, isCollapsed, toggleCollapse }">
-                    <slot
-                        :node="node"
-                        :depth="depth"
-                        :isLast="isLast"
-                        :ancestorHasNext="ancestorHasNext"
-                        :hasChildren="hasChildren"
-                        :isCollapsed="isCollapsed"
-                        :toggleCollapse="toggleCollapse" />
+            <TreeView v-for="(child, index) in children" :key="child.id" :node="child" :depth="childDepth"
+                :ancestorHasNext="childAncestorHasNext" :isLast="index === children.length - 1">
+                <template #default="{ node }">
+                    <slot :node="node" />
                 </template>
             </TreeView>
         </div>
